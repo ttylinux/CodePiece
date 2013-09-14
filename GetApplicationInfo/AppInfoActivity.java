ac30c64fed3322d8e73fcf0e2f0040638548f889
albert <albertxiaoyu@163.com>
2013-09-14 15:27:06 +0800

    PackageInfo--Get
    获取已经安装的所有应用信息

--- GetApplicationInfo/AppInfoActivity.java
+++ GetApplicationInfo/AppInfoActivity.java
@@ -1,6 +1,60 @@
 package com.example.android.home;
 
-public class AppInfoActivity
-{
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 
+public class AppInfoActivity extends Activity
+{
+	 private static ArrayList<ApplicationInfo> mApplications;
+	
+	
+	/**
+     * 系统上所有已经安装的应用的信息，封装在ResolveInfo中。
+     * ResolveInfo中的ActivityInfo(封装的Activity,BrocadcastReciever信息)；
+     * ServiceInfo(封装Service信息)
+     * 
+     */
+    private void loadApplications(boolean isLaunching) {
+        if (isLaunching && mApplications != null) {
+            return;
+        }
+
+        PackageManager manager = getPackageManager();
+
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        final List<ResolveInfo> apps = manager.queryIntentActivities(mainIntent, 0);
+        Collections.sort(apps, new ResolveInfo.DisplayNameComparator(manager));
+
+        if (apps != null) {
+            final int count = apps.size();
+
+            if (mApplications == null) {
+                mApplications = new ArrayList<ApplicationInfo>(count);
+            }
+            mApplications.clear();
+
+            for (int i = 0; i < count; i++) {
+                ApplicationInfo application = new ApplicationInfo();
+                ResolveInfo info = apps.get(i);
+
+                application.title = info.loadLabel(manager);
+                application.setActivity(new ComponentName(
+                        info.activityInfo.applicationInfo.packageName,
+                        info.activityInfo.name),
+                        Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+                application.icon = info.activityInfo.loadIcon(manager);
+
+                mApplications.add(application);
+            }
+        }
+    }
 }
