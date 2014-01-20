3097ecdfa7a578abe86a48d1d17265bbf8b4d2d8
albertxiaoyu <albertxiaoyu@163.com>
2014-01-20 22:33:40 +0800

    小修改
    小修改。

--- AndroidRelated/CompressAndDisplay/src/com/compressanddisplay/tool/ExternalStorage.java
+++ AndroidRelated/CompressAndDisplay/src/com/compressanddisplay/tool/ExternalStorage.java
@@ -6,57 +6,48 @@ import android.os.Environment;
 
 /**
  * 
- *外部存储器管理方面 
+ * 外部存储器管理方面
  */
 public class ExternalStorage {
 
 	public static String imgType = Environment.DIRECTORY_PICTURES;
-	
+
 	/**
-	 *检查外部存储器是否可用(可读写；有外部存储器)
-	 *如果可用，则返回true；不可用则返回false
+	 * 检查外部存储器是否可用(可读写；有外部存储器) 如果可用，则返回true；不可用则返回false
 	 * 
 	 */
-	
-	public ExternalStorage()
-	{
-		
-	}
-	public  boolean checkExternalStorage()
-	{
 
-		boolean readWrite = false;
+	public ExternalStorage() {
+
+	}
+
+	public boolean checkExternalStorage() {
+
 		String state = Environment.getExternalStorageState();
 
-		if (Environment.MEDIA_MOUNTED.equals(state))
-		{
-			readWrite = true;
+		if (Environment.MEDIA_MOUNTED.equals(state)) {
+			return true;
+		} else {
+			return false;
 		}
-
-		return readWrite;
 	}
-	
-	
+
 	/**
 	 * 
-	 *返回外部存储器的所指向的一个外部目录
-////	 *@param  type--根据类型，Android会为你自动生成；在本次项目中，
-//	 *可以使用Environment.DIRECTORY_PICTURES类型
-	 *不使用这种方式，有些系统未必会为你自动生成。采用直接获取外部存储器的绝对路径的方式。
+	 * 返回外部存储器的所指向的一个外部目录 //// *@param type--根据类型，Android会为你自动生成；在本次项目中， //
+	 * *可以使用Environment.DIRECTORY_PICTURES类型
+	 * 不使用这种方式，有些系统未必会为你自动生成。采用直接获取外部存储器的绝对路径的方式。
 	 * 
 	 */
-	public  File getExterDir()
-	{
-		if(checkExternalStorage())
-		{
-			//使用API 1.0的方法。
-			File file =  Environment.getExternalStorageDirectory();
+	public File getExterDir() {
+		if (checkExternalStorage()) {
+			// 使用API 1.0的方法。
+			File file = Environment.getExternalStorageDirectory();
 			return file;
-			
-		}else
-		{
+
+		} else {
 			return null;
 		}
 	}
-	
+
 }
